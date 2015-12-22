ea7f2df133cceedce8364d07d5825ca7d7debf96
kanchhi <ashish.adh@gmail.com>
2015-12-22 09:56:21 -0600

    a vanilla version of drag and drop without any angular in the components folder

--- components/dragDrop/app.js
+++ components/dragDrop/app.js
@@ -2,49 +2,49 @@ var app 		= angular.module('homepage', []);
 var operations 	= require('../operations.js');
 var setEngine 	= require('../setEngine.js');
 
-app.controller("proofController", function($scope){
-	this.oldSets	 = [];
-	this.elements	 = [];
-	this.newSets	 = [];
-	this.selectedSets = [];
-	this.groupname = 'group';
+// // app.controller("proofController", function($scope){
+// // 	this.oldSets	 = [];
+// // 	this.elements	 = [];
+// // 	this.newSets	 = [];
+// // 	this.selectedSets = [];
+// // 	this.groupname = 'group';
 
-	var A = new setEngine.Set(this.groupname, 'A');
-	var B = new setEngine.Set(this.groupname, 'B');
-	var C = new setEngine.Set(this.groupname, 'C');
-	var x = new setEngine.Element('x', A);
-	var y = new setEngine.Element('y', B);
-	var z = new setEngine.Element('z', C);
+// 	var A = new setEngine.Set(this.groupname, 'A');
+// 	var B = new setEngine.Set(this.groupname, 'B');
+// 	var C = new setEngine.Set(this.groupname, 'C');
+// 	var x = new setEngine.Element('x', A);
+// 	var y = new setEngine.Element('y', B);
+// 	var z = new setEngine.Element('z', C);
 
-	this.oldSets.push(A);
-	this.oldSets.push(B);
-	this.oldSets.push(C);
+// 	this.oldSets.push(A);
+// 	this.oldSets.push(B);
+// 	this.oldSets.push(C);
 
-	this.elements.push(x);
-	this.elements.push(y);
-	this.elements.push(z);
+// 	this.elements.push(x);
+// 	this.elements.push(y);
+// 	this.elements.push(z);
 
-	// this.drag = function (ev){
-	// ev.dataTransfer.setData('text/html', ev.target);
-	// };
+// 	// this.drag = function (ev){
+// 	// ev.dataTransfer.setData('text/html', ev.target);
+// 	// };
 
-	// this.drop = function (ev){
-	// 	ev.preventDefault();
-	// 	var data  = ev.dataTransfer.getData('text/html');
-	// 	ev.target.appendChild(document.getElementById(data));
-	// };
+// 	// this.drop = function (ev){
+// 	// 	ev.preventDefault();
+// 	// 	var data  = ev.dataTransfer.getData('text/html');
+// 	// 	ev.target.appendChild(document.getElementById(data));
+// 	// };
 
-	// this.dragover = function (ev){
-	// 	ev.preventDefault();
-	// };
+// 	// this.dragover = function (ev){
+// 	// 	ev.preventDefault();
+// 	// };
 
 
 
-	this.clicked = function (set){
-		console.log("clicked");
-		console.log("Set: " + set)
-		newSets.push(set);
-		console.log("new sets: "+ newSets);
-		console.log("selectedSets: " +selectedSets);
-	}
-});
+// 	this.clicked = function (set){
+// 		console.log("clicked");
+// 		console.log("Set: " + set)
+// 		newSets.push(set);
+// 		console.log("new sets: "+ newSets);
+// 		console.log("selectedSets: " +selectedSets);
+// 	}
+// });
--- components/dragDrop/drag.js
+++ components/dragDrop/drag.js
@@ -1,14 +1,20 @@
 function drag(ev){
-	ev.dataTransfer.setData('text/html', ev.target);
+	ev.dataTransfer.setData('text', ev.target.id);
+	console.log("drag fired");
 };
 
 function drop(ev){
+	console.log("drop started");
 	ev.preventDefault();
-	var data  = ev.dataTransfer.getData('text/html');
-	ev.target.(document.getElementById(data));
+	var data  = ev.dataTransfer.getData('text');
+	console.log(data + " :data");
+	ev.target.appendChild(document.getElementById(data));
+	console.log("drop fired");
+
 };
 
 function dragover(ev){
 	ev.preventDefault();
+	console.log("dragover fired");
 };
 
